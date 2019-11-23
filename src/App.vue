4ba6dcdb52a3ce75d96a31ad57e6f486bb213221
XSrDanDanX <ddipaumirim@gmail.com>
2019-11-23 18:57:54 -0300

    Modificações nas paginas de noticias sobre tecnologia e jogos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="app">
+      <NavBar v-bind:links="navbar.links" v-bind:urlLogo="navbar.urlLogo"></NavBar>
       <router-view></router-view>
   </div>
 </template>
 <script>
+import NavBar from './components/NavBar'
+
 export default {
   name: 'app',
+  components: {
+    NavBar
+  },
+
+  data: ()=>{
+    return {
+      navbar: {
+      links: [
+        {nome: 'Jogos', caminho: '/jogos'},
+        {nome: 'Cinema', caminho: '/cinema'},
+        {nome: 'Vestibular', caminho: '/vestibular'},
+      ],
+        urlLogo: 'https://vuejs.org/images/logo.png'
+      }
+    }
+  }
+  ,
+
   
 }
 </script>
--- src/pages/Jogos.vue
+++ src/pages/Jogos.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="app">
-        <NavBar v-bind:links="navbar.links" v-bind:urlLogo="navbar.urlLogo"></NavBar>
         <div class="row">
             <div class="col s12 m4">
                 <Container v-bind:noticias="noticias" tituloContainer="Últimas Noticias"></Container>
@@ -14,24 +13,14 @@
 </template>
 <script>
 
-import NavBar from '../components/NavBar'
 import Container from '../components/Container'
 export default {
     name: 'Jogos',
     components: {
-        NavBar,
         Container
     },
     data: () => {
         return {
-            navbar: {
-                links: [
-                    {nome: 'Jogos', caminho: '/jogos'},
-                    {nome: 'Cinema', caminho: '/cinema'},
-                    {nome: 'Vestibular', caminho: '/vestibular'},
-                ],
-                urlLogo: 'https://vuejs.org/images/logo.png'
-            },
             noticias: [
                 {titulo: 'Titulo1', descricao: 'Descricao1', link: '#'},
                 {titulo: 'Titulo2', descricao: 'Descricao2', link: '#'},
